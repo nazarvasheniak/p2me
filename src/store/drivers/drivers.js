51f69249fec0352dfcd15f5960d13f756ab0eb3f
Nazar Vasheniak <nazar.vasheniak@gmail.com>
2020-04-06 20:34:20 +0300

    drivers online

--- src/store/drivers/drivers.js
+++ src/store/drivers/drivers.js
@@ -11,24 +11,24 @@ export default {
     },
     actions: {
         loadDriversList(context, filter) {
-            const formData = new FormData();
-            formData.set('filter', filter);
-
-            axios.get(`/api/superadmin/drivers_list/`, {
-                headers: {
-                    "Access-Control-Allow-Origin": '*',
-                    "Content-Type": "application/json",
-                    "X-CSRFToken": "j8PYQSsFNEHgI0qclM5zcMyCUH3vepQR9LEnZVut36UZ7K5XdWVDsVkkFLrgaySG",
-                    Authorization: 'token ' + localStorage.getItem('token')
-                },
-                data: formData
-            })
-                .then(response => {
-                    context.commit('LOAD_DRIVERS_LIST', response.data.drivers_list);
+            return new Promise((resolve, reject) => {
+                axios.get('/api/superadmin/drivers_list/?filter=' + filter, {
+                    headers: {
+                        "Access-Control-Allow-Origin": '*',
+                        "Content-Type": "application/json",
+                        "X-CSRFToken": "j8PYQSsFNEHgI0qclM5zcMyCUH3vepQR9LEnZVut36UZ7K5XdWVDsVkkFLrgaySG",
+                        Authorization: 'token ' + localStorage.getItem('token')
+                    }
                 })
-                .catch(e => {
-                    console.log(e);
-                });
+                    .then(response => {
+                        context.commit('LOAD_DRIVERS_LIST', response.data.drivers_list);
+                        resolve(response.data);
+                    })
+                    .catch(e => {
+                        console.log(e);
+                        reject(e);
+                    });
+            });
         }
     },
     getters: {
